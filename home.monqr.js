0815348a2e4244ac2493dc031155fba15c309b8b
dnsas <saifoulayediallo250@gmail.com>
2024-11-03 19:05:00 +0100

    ajout d ela fonction pop up

--- home.monqr.js
+++ home.monqr.js
@@ -1,3 +1,11 @@
+function openPopup() {
+  document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup() {
+  document.getElementById('overlay').style.display = 'none';
+}
+
 function generateQRCode() {
   // Récupération des valeurs des champs
   const nom = document.querySelector('.nom').value.trim();
